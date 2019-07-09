435abe2ea71cfa9f91c9ce13688e4549f08ca227
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:47:10 +0400

    Add files via upload

---
+++ sketch_jun28d.ino
@@ -0,0 +1,15 @@
+int led = 8;
+int butt=13;
+void setup() {
+
+  pinMode(led, OUTPUT);
+  pinMode(butt, INPUT);
+}
+
+void loop() {
+if(digitalRead(butt)==HIGH){
+  digitalWrite(led,HIGH);
+  }else{
+     digitalWrite(led,LOW);
+    }
+}
