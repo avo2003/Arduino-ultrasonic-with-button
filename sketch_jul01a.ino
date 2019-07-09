5c495e886135fe8952b409ebecd2f4c5a8d0bf8a
avo2003 <50109997+avo2003@users.noreply.github.com>
2019-07-10 01:32:12 +0400

    Add files via upload

---
+++ sketch_jul01a.ino
@@ -0,0 +1,43 @@
+int trigPin = 9;
+int echoPin = 10;
+int led = 7;
+int button = 12;
+int type = 0;
+
+void setup() {
+  Serial.begin(9600); 
+   pinMode(led, OUTPUT);
+   pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(button, INPUT);
+  
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(1000);
+  digitalWrite(trigPin, LOW);
+  duration=pulseIn(echoPin, HIGH);
+  distance =(duration/2)/29.1;
+  Serial.print(distance);
+  Serial.println("CM");
+  delay(10);
+
+ if(digitalRead(button) == HIGH && type == 0){
+  type += 1;
+  delay(500);
+  }
+  if(digitalRead(button) == HIGH && type == 1){
+  type -= 1;
+  delay(500);
+  }
+ if(distance<=10 && type == 1) 
+  {
+    digitalWrite(led, HIGH);
+}
+   else if(distance>10)
+ {
+     digitalWrite(led, LOW);
+   }
+}
